fcbc76ca7729b2858db5099397c646f9922f1758
dstupski <dstupski@uw.edu>
2025-03-26 12:35:13 -0700

    daqnodeand relevant published

--- ros2daq/daqNode.py
+++ ros2daq/daqNode.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+import time
+from std_msgs.msg import Float32MultiArray, Header
 #from magnotether.magnotether import AnalogInputs  # Import the custom message
-from uldaq import get_daq_device_inventory, DaqDevice, InterfaceType, AiInputMode
-
+from uldaq import get_daq_device_inventory, DaqDevice, InterfaceType, AiInputMode, AInFlag
+#from ros2daq.msg import AnalogInputs
 
 class AnalogPublisher(Node):
     def __init__(self):
@@ -23,32 +25,36 @@ class AnalogPublisher(Node):
         ai_info = self.ai_device.get_info()
         self.ai_range = ai_info.get_ranges(AiInputMode.SINGLE_ENDED)[0]
 
-        self.publisher_ = self.create_publisher(AnalogInputs, 'analog_inputs', 10)
+        self.publisher_ = self.create_publisher(Float32MultiArray, 'analog_inputs', 10)
         self.timer = self.create_timer(.01, self.publish_analog_data)  # Publish every 10ms
 
     def publish_analog_data(self):
         if not self.daq_device.is_connected():
             self.get_logger().error("DAQ device not connected.")
             return
-        data =[0]*8
+        data =[0.]5
         msg = msg = Float32MultiArray()
 
         try:
+            flags = AInFlag.DEFAULT
             # Read analog inputs from channels 0 to 7
-            data[0] = self.ai_device.a_in(0, AiInputMode.SINGLE_ENDED, self.ai_range)
-            data[1] = self.ai_device.a_in(1, AiInputMode.SINGLE_ENDED, self.ai_range)
-            data[2] = self.ai_device.a_in(2, AiInputMode.SINGLE_ENDED, self.ai_range)
-            data[3] = self.ai_device.a_in(3, AiInputMode.SINGLE_ENDED, self.ai_range)
-            data[4] = self.ai_device.a_in(4, AiInputMode.SINGLE_ENDED, self.ai_range)
-            data[5] = self.ai_device.a_in(5, AiInputMode.SINGLE_ENDED, self.ai_range)
-            data[6] = self.ai_device.a_in(6, AiInputMode.SINGLE_ENDED, self.ai_range)
-            data[7] = self.ai_device.a_in(7, AiInputMode.SINGLE_ENDED, self.ai_range)
+            #t1 =time.time()
+            data[0] = (time.time())
+            data[1] = self.ai_device.a_in(0, AiInputMode.SINGLE_ENDED, self.ai_range, flags)
+            data[2] = self.ai_device.a_in(1, AiInputMode.SINGLE_ENDED, self.ai_range, flags)
+            data[3] = self.ai_device.a_in(2, AiInputMode.SINGLE_ENDED, self.ai_range, flags)
+            #data[4] = self.ai_device.a_in(3, AiInputMode.SINGLE_ENDED, self.ai_range, flags)
+           # data[5] = self.ai_device.a_in(4, AiInputMode.SINGLE_ENDED, self.ai_range, flags)
+            #data[6] = self.ai_device.a_in(5, AiInputMode.SINGLE_ENDED, self.ai_range, flags)
+            #data[7] = self.ai_device.a_in(6, AiInputMode.SINGLE_ENDED, self.ai_range, flags)
+           # data[8] = self.ai_device.a_in(7, AiInputMode.SINGLE_ENDED, self.ai_range, flags)
+            #t2 = time.time()
+            print(t2-t1)
+            #msg.header.stamp = self.get_clock().now().to_msg()
             msg.data=data
             self.publisher_.publish(msg)
-            self.get_logger().info(
-                f'Published AI Voltages: [{msg.AI0:.2f}, {msg.AI1:.2f}, {msg.AI2:.2f}, '
-                f'{msg.AI3:.2f}, {msg.AI4:.2f}, {msg.AI5:.2f}, {msg.AI6:.2f}, {msg.AI7:.2f}]'
-            )
+            #self.get_logger().info(f'Published: {data}')
+
 
         except Exception as e:
             self.get_logger().error(f"Error reading from DAQ device: {e}")
--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +21,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        'daqNode = ros2daq.daqNode:main'
         ],
     },
+
 )
